6b8f14e8ceb9e31da356495df20da8a3af4d6da8
Maksym406 <maxair658@gmail.com>
2024-10-07 15:28:39 +0200

    hw6

---
+++ src/app/Homewk6.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Homewk6 {
+
+    public static void main(String[] args) {
+        int totalSum = 0;
+        for (int i = 1; i <= 6; i++) {
+            totalSum += i;
+
+            System.out.println(i + ") Num is " + i + ", sum is " + totalSum);
+        }
+
+        System.out.println("------------------------");
+
+
+        System.out.println("Sum of numbers is " + totalSum);
+    }
+}
